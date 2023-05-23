1b78961ad4265cccf8d2fd69996523116bde73d5
Avi <avishekuser@gmai.com>
2023-05-23 21:20:39 +0600

    api added

--- index.js
+++ index.js
@@ -25,13 +25,24 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
+        const  toysCollection = client.db('toysAnimal').collection('toysInformation');
+
+        app.get('/toys',async(req,res)=>{
+            const cursor = toysCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/toys_by_category',(req,res)=>{
+            const category = req.query.category;
+        })
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
